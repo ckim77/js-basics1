9e97daa88cb6960fb88e608c47965d21ac3f25b1
Chris Kim <ckjr92@gmail.com>
2021-10-15 10:32:51 -0600

    finished the password problem, starting on the going further points

--- password.js
+++ password.js
@@ -1,3 +1,19 @@
 /// Assessment 1: Password
 
 console.log('Welcome to the password validator tool.')
+
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+reader.question("Please enter your password: ", function(password) {
+    if (password.length >= 10) {
+      console.log("Success!");
+    } else {
+      console.log('Failure, please try again.');
+    }
+  });
+
